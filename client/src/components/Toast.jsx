2f2a349f9a27e32a0a86fe89556823e74a79fd2f
Your Name <manalch4015@gmail.com>
2026-09-09 15:46:04 +0500

    priority

--- client/src/components/Toast.jsx
+++ client/src/components/Toast.jsx
@@ -23,7 +23,7 @@ export const ToastProvider = ({ children }) => {
     <ToastContext.Provider value={{ showToast }}>
       {children}
 
-      <div className="fixed bottom-4 right-4 z-[100] flex flex-col gap-2">
+      <div className="fixed bottom-4 right-4 z-100 flex flex-col gap-2">
         {toasts.map((t) => {
           const style = STYLES[t.type] || STYLES.info;
           return (
